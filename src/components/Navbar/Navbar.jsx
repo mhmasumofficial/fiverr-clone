eceaf44cd7fe43bf4df19b25ea2bbabc06231266
mhmasumofficial <mhmasum4048@gmail.com>
2023-07-25 12:52:55 +0600

    working on navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.scss";
 
 function Navbar() {
+  const [active, setActive] = useState(false);
+
+  
+
   return (
-    <div className="navbar">
+    <div className={active ? "navbar active" : "navbar"}>
       <div className="container">
         <div className="wrapper">
           <div className="logo">
@@ -19,6 +23,19 @@ function Navbar() {
             <button>Join</button>
           </div>
         </div>
+      </div>
+
+      <hr />
+      <div className="menu">
+        <span>Graphics & Design</span>
+        <span>Video & Animation</span>
+        <span>Writing & Translation</span>
+        <span>AI Services</span>
+        <span>Digital Marketing</span>
+        <span>Music & Audio</span>
+        <span>Programming & Tech</span>
+        <span>Business</span>
+        <span>Lifestyle</span>
       </div>
     </div>
   );
